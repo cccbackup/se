92e96525f9ba7e8dc3abce549234459bbb7a4322
ccckmit <ccckmit@gmail.com>
2020-06-04 10:02:58 +0800

    simplify Cnk

--- algorithm/05-dynamicProgramming/combinatorial/CnkDynamic.js
+++ algorithm/05-dynamicProgramming/combinatorial/CnkDynamic.js
@@ -3,23 +3,20 @@
 // https://en.wikipedia.org/wiki/Pascal%27s_triangle
 // https://en.wikipedia.org/wiki/Binomial_coefficient
 /*
-c(n, k) = 0                        , if n < k
-        = 1                        , if k = 0 or k = n
+c(n, k) = 1                        , if k = 0 or k = n
         = c(n-1, k-1) + c(n-1, k)  , if k <= n-k
-        = c(n, n-k)                , if k > n-k
 */
 
 function c(N, K) {
   var C = [];
-  for (let n=0; n<=N; n++) C[n] = [1]
+  for (let n=0; n<=N; n++) {
+    C[n] = []
+    C[n][0] = 1
+    C[n][n] = 1
+  }
   for (let n=1; n<=N; n++) {
-    for (let k=1; k<=N; k++) {
-      if (n < k)
-        C[n][k] = 0
-      else if (n===k)
-        C[n][k] = 1
-      else
-        C[n][k] = C[n-1][k-1] + C[n-1][k]
+    for (let k=1; k<n; k++) {
+      C[n][k] = C[n-1][k-1] + C[n-1][k]
     }
   }
   for(let n=0; n<=N; n++) {
